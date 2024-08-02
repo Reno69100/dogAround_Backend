e1ee7848945b8a18de6422bb8352960730d7e296
Mathieu <m.hembert@outlook.fr>
2024-08-02 17:14:28 +0200

    Modif connection.js connection string

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://llagosta:5EFxj7cB3I8xj8Mx@cluster0.wninfmo.mongodb.net/dogaround';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, {connectTimeoutMs: 2000})
 	.then(() => console.log('Data Connected'))
