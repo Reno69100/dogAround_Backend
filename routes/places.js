cc78834e5a46fa55df967fcc08464e4d11e5dbe6
Mathieu <m.hembert@outlook.fr>
2024-08-06 12:02:17 +0200

    modifications routes places

--- routes/places.js
+++ routes/places.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var router = express.Router(); 
+var router = express.Router();
 const fetch = require('node-fetch');
 
 // const apiKey = 'AIzaSyD7kmefnloIc3Ank2T2wa5Qut4MohDNyTk';
@@ -13,39 +13,84 @@ const fetch = require('node-fetch');
 // https://places.googleapis.com/v1/places:searchNearby?locationRestriction={circle={center={latitute=${latitude},longitude=${longitude}},radius=${radius}}} 
 // const keywords = "dog-friendly park OR dog park OR dog-friendly beach OR dog-friendly trail OR dog-friendly hiking OR dog-friendly outdoor area OR dog-friendly lake OR dog-friendly nature reserve OR dog-friendly campground OR dog-friendly picnic area";
 
+
+//Route POST pour récupérer les points d'intérêts, et évenements autour de l'utilisateur
 router.post("/:latitude/:longitude/:radius", (req, res) => {
-  const placesTypes = ['park','dog_park','pet_store','restaurant','national_park','veterinary_care'];
-  //const placesTypes = ['park']
+  //Vérification des params de la route
+  if ((req.params.latitude === null) || (req.params.latitude === null) || (req.params.latitude === null)) {
+    res.status(400).json({ result: false, error: "problem route post places/:latitude/:longitude/:radius" });
+    return;
+  }
+
+  //Déclaration categories a recuperer sur Google maps
+  const placesTypes = ['park', 'dog_park', 'pet_store', 'restaurant', 'national_park', 'veterinary_care'];
+
+  //Ecriture query includedTypes
   let dataTypes = '';
-  for (const element of placesTypes){
+  for (const element of placesTypes) {
     dataTypes += `&includedTypes=${element}`
   }
+
+  //Ecriture globale query en fonction de la latitude, longitude, rayon autour de l'utilisateur, et la categories des places voulues
   const params = `locationRestriction.circle.center.latitude=${req.params.latitude}&locationRestriction.circle.center.longitude=${req.params.longitude}&locationRestriction.circle.radius=${req.params.radius}${dataTypes}`
 
+  //Requete API google
   fetch(`https://places.googleapis.com/v1/places:searchNearby?${params} `, {
     method: 'POST',
     headers: {
-        'Content-Type': 'application/json', 
-        'X-Goog-Api-Key': process.env.GOOGLE_API_KEY,
-        'X-Goog-FieldMask': 'places.id,places.location,places.allowsDogs,places.types',
+      'Content-Type': 'application/json',
+      'X-Goog-Api-Key': process.env.GOOGLE_API_KEY,
+      'X-Goog-FieldMask': 'places.id,places.location,places.allowsDogs,places.types',
     },
   })
-  .then(response => response.json())
-  .then(data => {
-    // const places = data.places.filter(e=>e.displayName.languageCode='fr')
-    const places = data.places.map(e=>{
-      const newPlace = {
-        google_id: e.id,
-        location: e.location,
-        types: e.types.filter(x=>{
-        if(placesTypes.some(element=> element === x)){
-          return x;
+    .then(response => response.json())
+    .then(data => {
+      //Déclaration categories côté frontend
+      const categories = [
+        ['park', "parc"],
+        ['dog_park', "parc"],
+        ['national_park', "parc"],
+        ['pet_store', "animalerie"],
+        ['restaurant', "restaurant"],
+        ['veterinary_care', "veterinaire"],
+        ['', "air"],
+        ['', "eau"],
+        ['', "like"],
+        ['', "favori"],
+        ['', "event"],
+      ];
+
+      //Mise en forme des données à renvoyer au front end
+      const places = data.places.map(e => {
+
+        //Conversion du type google en type utilsable côté frontend
+        let typefilter = '';
+        for (const googletype of e.types) {
+          for (const element of categories) {
+            if (googletype.toLowerCase() === element[0].toLowerCase()) {
+              typefilter = element[1].toLowerCase();
+              /* console.log(typefilter); */
+              break;
+            }
+          }
         }
-      })}
-      return newPlace
+
+        //Schema objet nouvelle places à renovyer au frontend
+        const newPlace = {
+          id: '',
+          google_id: e.id,
+          location: e.location,
+          type: typefilter,
+          events: [],
+          likes: [],
+        }
+
+        return newPlace;
+      })
+
+      //Réponse route
+      res.status(200).json({ result: true, places });
     })
-    res.status(200).json({ result: true, places});
-  })
 })
 
 module.exports = router;
@@ -54,21 +99,21 @@ module.exports = router;
 //,'dog_park','pet_store','restaurant','national_park','veterinary_care'
 //encodeURIComponent(keyword)
 
-  // const keywords = [
-  //   "dog-friendly swimming area",
-  //   "dog-friendly lake",
-  //   "dog-friendly beach",
-  //   "dog-friendly pond",
-  //   "dog-friendly river",
-  //   "dog-friendly public park",
-  //   "dog-friendly park",
-  //   "dog-friendly private park",
-  //   "private dog park",
-  //   "dog area",
-  //   "dog park",
-  //   "dog-friendly area",
-  //   "dog-friendly trail",
-  //   "dog-friendly hiking",
-  //   "dog walking area",
-  //   "dog-friendly outdoor area"
-  // ];
+// const keywords = [
+//   "dog-friendly swimming area",
+//   "dog-friendly lake",
+//   "dog-friendly beach",
+//   "dog-friendly pond",
+//   "dog-friendly river",
+//   "dog-friendly public park",
+//   "dog-friendly park",
+//   "dog-friendly private park",
+//   "private dog park",
+//   "dog area",
+//   "dog park",
+//   "dog-friendly area",
+//   "dog-friendly trail",
+//   "dog-friendly hiking",
+//   "dog walking area",
+//   "dog-friendly outdoor area"
+// ];
